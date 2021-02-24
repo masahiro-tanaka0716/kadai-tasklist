af9dd4f9b58392740f274078571624da0ad16579
masahiro-tanaka0716 <s1377062@gmail.com>
2021-02-24 08:55:33 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,25 +1,31 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all
+    if logged_in?
+      @tasks = current_user.tasks.order(id: :desc)
+    end
   end
   
   def show
   end
   
   def new
-    @task = Task.new
+    if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+    end
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
-      flash[:success] = 'Task が正常に投稿されました'
-      redirect_to @task
+      flash[:success] = 'タスクを登録しました。'
+      redirect_to root_url
     else
-      flash.now[:danger] = 'Task が投稿されませんでした'
+      flash.now[:danger] = 'タスクを登録できませんでした。'
       render :new
     end
   end
@@ -28,11 +34,12 @@ class TasksController < ApplicationController
   end
   
   def update
+    
     if @task.update(task_params)
-      flash[:success] = 'Task は正常に更新されました'
+      flash[:success] = 'タスクを変更しました。'
       redirect_to @task
     else
-      flash.now[:danger] = 'Task は更新されませんでした'
+      flash.now[:danger] = 'タスクを更新できませんでした。'
       render :edit
     end
   end
@@ -40,20 +47,24 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     
-    flash[:success] = 'Task は正常に削除されました'
+    flash[:success] = 'タスクを削除しました。'
     redirect_to tasks_url
   end
   
   private
   
-  
   def set_task
     @task = Task.find(params[:id])
   end
   
-  
   def task_params
-    params.require(:task).permit(:content, :status)
+    params.require(:task).permit(:status, :content)
   end
   
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
+
   def index
-    @users = User.order(id: :desc).page(params[:id]).per(10)
   end
 
   def show
